44676edbef64c142bde0cd60501187f33f076c5a
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-24 14:44:22 +0100

    Esercizio finito - Sperimento altre funzionalità.
    Il comando base dell'esercizio è finito; provo a vedere di aggiungere un button che richiama qualche funzione o dei campi input per testare il V-Model

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 
-alert("Hello World!")
+
+
+/*Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
+
+// INIZIALIZZO VUE dopo aver linkato la CDN nell HTML:
+
+const App = new Vue (
+
+  {
+    // con El collego le istruzioni di App al #root nell HTML
+    el: '#root',
+
+    //  In Data inserisco tutti i dati che voglio iniettare in HTML
+    data: {
+
+      title: 'HELLO VUE JS !!!',
+      imgLink: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+
+
+    },
+
+  },
+
+);
