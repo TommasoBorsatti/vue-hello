b6bd47ba5399f0e83728149d771c5a9eb949d692
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-24 15:47:41 +0100

    Script VUE JS - Aggiunta una funzionalità al click di un buttone.
    Tramite il V-Bind sono definite delle classi colore a diversi elementi html; un button è messo in ascolto @click e richiama una funzione che controlla le classi inserite nell Oggetto vue, e procede a riassegnarle ad altre classi CSS in modo da cambiare la palette della pagina html.

--- js/script.js
+++ js/script.js
@@ -20,7 +20,23 @@ const App = new Vue (
 
       title: 'HELLO VUE JS !!!',
       imgLink: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+      palette1: 'palette1',
+      palette2: 'palette2',
+      palette3: 'palette3',
+      palette4: 'palette4',
+    },
 
+    //  In Data inserisco tutte le funzioni che voglio richiamare tramite eventi.
+    methods: {
+
+      messColorsUp: function() {
+
+        if (App.palette1 == "palette1") {
+          return (App.palette1 = "palette5",  App.palette2 = "palette6", App.palette3 = "palette7", App.palette4 = "palette8");
+        } else {
+          return (App.palette1 = 'palette1', App.palette2 = "palette2", App.palette3 = "palette3", App.palette4 = "palette4");
+        }
+      },
 
     },
 
